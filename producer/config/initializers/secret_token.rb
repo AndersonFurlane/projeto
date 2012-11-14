1f3db4cc25306506786b3e5feb9bf4f57e6e3084
AnderonFurlane <andersonfurlanemartins@gmail.com>
2012-11-13 23:36:48 -0200

    Recomeço

--- producer/config/initializers/secret_token.rb
+++ producer/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Producer::Application.config.secret_token = '2f825898bf947ddb3f37b2445372b6d1ef1da2b99feb591ae90721c818b2305bef7af87244d03dff68ecad4ab1b2e65a0b7f7df996820fdc2eeaefb8a3972d0c'
+Producer::Application.config.secret_token = '15742a72314a47069e8ad660bd385504e6bc326ca0bd7d8c2de712c972a5bcc48db837e2ed3c5d141e59e7a392aef0fa3fa4f6b60c2df2e4dc5e6a47db8b2743'
